b6997fb6051def290b3f6e275a15f9b3bb876be5
Jordi Romero <jordi@jrom.net>
2013-06-21 14:16:30 +0200

    add loggers

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express'),
 
 var app = express();
 app.use(express.static('public'));
+app.use(express.logger());
 
 app.engine('html', require('ejs').renderFile);
 
@@ -24,10 +25,15 @@ app.get('/gif/:id', function (req, res) {
       videopath = tmpdir + id + '.mp4';
 
   fs.exists(gifpath, function (exists) {
+    console.log('GIF ' + id + ': checking if we already have it');
     if (exists) {
+      console.log('GIF ' + id + ': bingo! We have it, streaming...');
       res.sendfile(gifpath);
     }
     else {
+      console.log('GIF ' + id + ': shoot.. apparently this is the first time someone wants this gif');
+      console.log('GIF ' + id + ': fetching video URL');
+
       var command1 = 'ffmpeg -i ' + videopath + ' -r 10 ' + tmpdir + id + '%05d.png',
       command2 = 'convert -colors 16 ' +  tmpdir + id + '*.png ' + gifpath,
       command3 = 'rm -f ' + tmpdir + id + '*.png ' + videopath,
@@ -45,12 +51,17 @@ app.get('/gif/:id', function (req, res) {
             url = data.join('').match(/\<source src="([^"]*)"/)[1];
 
             file = fs.createWriteStream(videopath);
+            console.log('GIF ' + id + ': downloading video');
             request = https.get(url, function (response) {
               response.pipe(file);
               response.on('end', function () {
+                console.log('GIF ' + id + ': creating PNGs');
                 var child1 = exec(command1, function (error, stdout, stderr) {
+                  console.log('GIF ' + id + ': creating GIF');
                   var child2 = exec(command2, function (error, stdout, stderr) {
+                    console.log('GIF ' + id + ': removing temporary PNGs');
                     var child3 = exec(command3, function (error, stdout, stderr) {
+                      console.log('GIF ' + id + ': streaming GIF');
                       res.sendfile(gifpath);
                     });
                   });
