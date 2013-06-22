79c4eefab15d5b420ff6f6934f60383ccad11861
Jordi Romero <jordi@jrom.net>
2013-06-22 03:22:28 +0200

    super friday night refactor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express'),
     https = require('https');
 
 var app = express();
-app.use(express.static('public'));
+app.use(express['static']('public'));
 app.use(express.logger());
 
 app.engine('html', require('ejs').renderFile);
@@ -18,65 +18,92 @@ app.get('/v/:id', function (req, res) {
 });
 
 app.get('/gif/:id', function (req, res) {
-  var id = req.params.id,
-      tmpdir = 'tmp/',
-      gifdir = 'gif/',
-      gifpath = gifdir + id + '.gif',
-      videopath = tmpdir + id + '.mp4';
 
-  fs.exists(gifpath, function (exists) {
-    console.log('GIF ' + id + ': checking if we already have it');
-    if (exists) {
-      console.log('GIF ' + id + ': bingo! We have it, streaming...');
-      res.sendfile(gifpath);
-    }
-    else {
-      console.log('GIF ' + id + ': shoot.. apparently this is the first time someone wants this gif');
-      console.log('GIF ' + id + ': fetching video URL');
+  function send_gif(data) {
+    console.log('GIF ' + data.id + ': streaming GIF');
+    res.sendfile(data.gifpath);
+  }
 
-      var command1 = 'ffmpeg -i ' + videopath + ' -r 10 ' + tmpdir + id + '%05d.png',
-      command2 = 'convert -colors 16 ' +  tmpdir + id + '*.png ' + gifpath,
-      command3 = 'rm -f ' + tmpdir + id + '*.png ' + videopath,
-      video = https.get('https://vine.co/v/' + id + '/card', function (response) {
-        var url,
-            data = [],
-            file,
-            request;
-        response.on('data', function (chunk) {
-          data.push(chunk);
-        });
+  function clean_files(data) {
+    var command = 'rm -f ' + data.tmpdir + data.id + '*.png ' + data.videopath;
+    console.log('GIF ' + data.id + ': removing temporary PNGs');
+    exec(command, function (error, stdout, stderr) {
+      send_gif(data);
+    });
+  }
 
-        response.on('end', function () {
-          try {
-            url = data.join('').match(/\<source src="([^"]*)"/)[1];
+  function generate_gif(data) {
+    var command = 'convert -colors 16 ' +  data.tmpdir + data.id + '*.png ' + data.gifpath;
+    console.log('GIF ' + data.id + ': creating GIF');
+    exec(command, function (error, stdout, stderr) {
+      clean_files(data);
+    });
+  }
 
-            file = fs.createWriteStream(videopath);
-            console.log('GIF ' + id + ': downloading video');
-            request = https.get(url, function (response) {
-              response.pipe(file);
-              response.on('end', function () {
-                console.log('GIF ' + id + ': creating PNGs');
-                var child1 = exec(command1, function (error, stdout, stderr) {
-                  console.log('GIF ' + id + ': creating GIF');
-                  var child2 = exec(command2, function (error, stdout, stderr) {
-                    console.log('GIF ' + id + ': removing temporary PNGs');
-                    var child3 = exec(command3, function (error, stdout, stderr) {
-                      console.log('GIF ' + id + ': streaming GIF');
-                      res.sendfile(gifpath);
-                    });
-                  });
-                });
-              });
-            });
-          }
-          catch (e) {
-            res.redirect('/');
-          }
+  function extract_thumbnails(data) {
+    var command = 'ffmpeg -i ' + data.videopath + ' -r 10 ' + data.tmpdir + data.id + '%05d.png';
+    console.log('GIF ' + data.id + ': creating PNGs');
+    exec(command, function (error, stdout, stderr) {
+      generate_gif(data);
+    });
+  }
 
-        });
+  function fetch_video(data, url) {
+    var file = fs.createWriteStream(data.videopath);
+    console.log('GIF ' + data.id + ': downloading video');
+    https.get(url, function (response) {
+      response.pipe(file);
+      response.on('end', function () {
+        extract_thumbnails(data);
       });
-    }
-  });
+    });
+  }
+
+  function get_vine_url(data) {
+    console.log('GIF ' + data.id + ': fetching video URL');
+
+    https.get('https://vine.co/v/' + data.id + '/card', function (response) {
+      var url,
+          chunks = [],
+          file;
+      response.on('data', function (chunk) {
+        chunks.push(chunk);
+      });
+
+      response.on('end', function () {
+        try {
+          url = chunks.join('').match(/<source src="([^"]*)"/)[1];
+          fetch_video(data, url);
+        }
+        catch (e) {
+          res.redirect('/');
+        }
+      });
+    });
+  }
+
+  function serve_gif(data) {
+    fs.exists(data.gifpath, function (exists) {
+      console.log('GIF ' + data.id + ': checking if we already have it');
+      if (exists) {
+        console.log('GIF ' + data.id + ': bingo! We have it, streaming...');
+        res.sendfile(data.gifpath);
+      }
+      else {
+        console.log('GIF ' + data.id + ': shoot.. apparently this is the first time someone wants this gif');
+        get_vine_url(data);
+      }
+    });
+  }
+
+  var data        = {};
+  data.id         = req.params.id;
+  data.tmpdir     = 'tmp/';
+  data.gifdir     = 'gif/';
+  data.gifpath    = data.gifdir + data.id + '.gif';
+  data.videopath  = data.tmpdir + data.id + '.mp4';
+
+  serve_gif(data);
 });
 
 app.listen(4000);
