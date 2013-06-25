5d2b399bfd0cb5de10a9443059f516360a076604
Jordi Romero <jordi@jrom.net>
2013-06-25 15:04:41 +0200

    symlink gif and tmp

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,3 +20,25 @@ namespace :deploy do
   task :stop do ; end
 end
 
+set :normal_symlinks, %w(
+  gif
+  tmp
+)
+
+namespace :symlinks do
+  desc "Make all the damn symlinks"
+  task :make, :roles => :app, :except => { :no_release => true } do
+    commands = normal_symlinks.map do |path|
+      "rm -rf #{release_path}/#{path} && \
+       ln -s #{shared_path}/#{path} #{release_path}/#{path}"
+    end
+
+    run <<-CMD
+      cd #{release_path} &&
+      #{commands.join(" && ")}
+    CMD
+  end
+end
+
+after "deploy:update_code", "symlinks:make"
+
